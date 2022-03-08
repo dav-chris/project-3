db16d0a09667d7cfd27906bd95ef6a449ab310f3
David CHARLES-ELIE-NELSON <dvd.charles2004@gmail.com>
2022-03-08 15:36:51 +0100

    bug correction docker images

--- src/api/params.py
+++ src/api/params.py
@@ -5,5 +5,6 @@
 # Standard
 import os
 
+CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 
-SETTINGS_FILEPATH = os.path.join('conf', 'api_settings.json')
+SETTINGS_FILEPATH = os.path.join(CURRENT_PATH, 'conf', 'api_settings.json')
